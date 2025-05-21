263eb0a9859ca650a64891624025ceff2385242d
aryabimasantoso <aryabimasantoso21@gmail.com>
2025-05-21 12:36:24 +0700

    PERTEMUAN_12

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Link;
+use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\BlogController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -48,3 +51,16 @@ Route::get('danantara', function () {
 Route::get('ets', function () {
 	return view('ets');
 });
+
+Route::get('dosen', [Link::class, 'index'] );
+
+Route::get('/pegawai/{nama}', [PegawaiController::class, 'index'] );
+
+Route::get('/formulir', [PegawaiController::class, 'formulir'] );
+
+Route::post('/formulir/proses', [PegawaiController::class, 'proses'] );
+
+// route blog
+Route::get('/blog', [BlogController::class, 'home'] );
+Route::get('/blog/tentang', [BlogController::class, 'tentang'] );
+Route::get('/blog/kontak', [BlogController::class, 'kontak'] );
